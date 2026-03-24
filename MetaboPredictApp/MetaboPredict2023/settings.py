0f4f3c2cb681cc905685b57512686bcf0d627387
pierre <hushes-distant.0v@icloud.com>
2026-03-24 21:54:18 +0100

    STATICFILES_STORAGE

--- MetaboPredictApp/MetaboPredict2023/settings.py
+++ MetaboPredictApp/MetaboPredict2023/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ← add this
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
